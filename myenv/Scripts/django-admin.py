6c61509e5f7f96653ea5f96c1c09d6d133e60c4c
sh4mi <sh4mi@hotmail.com>
2020-03-09 19:22:03 +0500

    env moved

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\TEMP.LAPTOP-K19V4RB4\Downloads\atomic_mine\atomic_mine\myenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
